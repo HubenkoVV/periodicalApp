e9e9e3b9435048b80bd701938ea2408f2b223f35
HubenkoVV <hubenkovlada@gmail.com>
2018-04-30 18:28:59 +0300

    added constants

---
+++ periodicalProject/src/main/java/ua/training/util/constant/Commands.java
@@ -0,0 +1,16 @@
+package ua.training.util.constant;
+
+public interface Commands {
+    String LOGIN = "login";
+    String SIGNOUT = "sign_out";
+    String REGISTRATION = "registration";
+    String ADD_ARTICLE = "add_article";
+    String ADD_PERIODICAL = "add_periodical";
+    String TO_BASKET = "to_basket";
+    String ARTICLES = "articles";
+    String BUY_PERIODICAL = "subscribe";
+    String PERIODICALS = "periodicals";
+    String UPDATE_ACCOUNT = "update_account";
+    String SHOW_ARTICLE = "show_article";
+    String LANGUAGE = "language";
+}
---
+++ periodicalProject/src/main/java/ua/training/util/constant/Messages.java
@@ -0,0 +1,10 @@
+package ua.training.util.constant;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Messages {
+    public static final String ADD_PERIODICAL = "added.periodical";
+    public static final String ADD_ARTICLE = "added.article";
+    public static final String ADD_USER = "added.user";
+}
